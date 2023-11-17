3205c22c5be1114616a7c78651c5bcb1b4620c6e
MarceloMogli <moglimarcelo@gmail.com>
2023-11-17 00:47:16 +0000

    spinner added

--- src/components/FoodtruckDetails/FoodtruckDetail.jsx
+++ src/components/FoodtruckDetails/FoodtruckDetail.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import { InfinitySpin } from 'react-loader-spinner'
 
 const FoodTruckDetails = ({ data }) => {
-  if (!data) {
-    // Handle the case when data is not available
-    return <p>Loading...</p>;
-  }
+    if (!data) {
+        // Handle the case when data is not available
+        return <InfinitySpin width='200' color="#000000" />;
+    }
 
-  const { name, cuisine, location, menu } = data;
+    const { name, cuisine, location, menu } = data;
 
-  return (
-    <div>
-      <h2>{name}</h2>
-      <p>Cuisine: {cuisine}</p>
-      <p>Location: {location}</p>
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>Cuisine: {cuisine}</p>
+            <p>Location: {location}</p>
 
-      <h3>Menu:</h3>
-      <ul>
-        {menu.map((item, index) => (
-          <li key={index}>
-            {item.item} - {item.price}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            <h3>Menu:</h3>
+            <ul>
+                {menu.map((item, index) => (
+                    <li key={index}>
+                        {item.item} - {item.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default FoodTruckDetails;
